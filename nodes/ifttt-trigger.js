526e1c384322aaf6b7624770b9019311f1f53664
lucaspouchot <lucas.pouchot@gmail.com>
2019-07-24 18:18:02 +0200

    allow to send 0 as a value

--- nodes/ifttt-trigger.js
+++ nodes/ifttt-trigger.js
@@ -15,36 +15,36 @@ module.exports = function(RED) {
 
             this.val1 = msg.value1;
             if(typeof this.val1 === 'undefined'){
-              this.val1 = msg.payload
+              this.val1 = msg.payload;
             }
             if(typeof this.val1 === 'undefined'){
-              this.val1 = n.val1
+              this.val1 = n.val1;
             }
 
             this.val2 = msg.value2;
             if(typeof this.val2 === 'undefined'){
-              this.val2 = n.val2
+              this.val2 = n.val2;
             }
 
             this.val3 = msg.value3;
             if(typeof this.val3 === 'undefined'){
-              this.val3 = n.val3
+              this.val3 = n.val3;
             }
 
             var delimiter = '?'
             this.url = `https://maker.ifttt.com/trigger/${this.event}/with/key/${this.secretkey}`;
 
-            if (this.val1 != "") {
+            if (this.val1 !== "") {
                 this.url = `${this.url}${delimiter}value1=${this.val1}`;
-                delimiter = '&'
+                delimiter = '&';
             }
 
-            if (this.val2 != "") {
+            if (this.val2 !== "") {
                 this.url = `${this.url}${delimiter}value2=${this.val2}`;
-                delimiter = '&'
+                delimiter = '&';
             }
 
-            if (this.val3 != "") {
+            if (this.val3 !== "") {
                 this.url = `${this.url}${delimiter}value3=${this.val3}`;
             }
 
